74db16e3a23eff8ef6faf2a5c8a5475f69b69061
Mathys Rioux <mat.stjr@gmail.com>
2025-12-03 19:47:18 -0500

    fix typos

--- Formula/deluge.rb
+++ Formula/deluge.rb
@@ -6,7 +6,7 @@ class Deluge < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mstjr/deluge/releases/download/v0.1.0/deluge-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/mstjr/deluge/releases/download/0.1.0/deluge-aarch64-apple-darwin.tar.gz"
       sha256 "e1ac8c1e8f6b1d6aa4d7744acad15c1e6b3f07128c02aa775759f7c9b1c3757a"
     else
       odie "Intel Macs are not supported by this formula"
